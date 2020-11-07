9657353b39b54a5a0ed3651a2b4011d67a260286
dengxuan <dengxuan@mail.com>
2020-11-07 17:38:52 +0800

    移除Skywalker.Ddd.Application.Contracts对Skywalker.Lighting.Abstraction的依赖

--- src/Ddd/src/Skywalker.Ddd.Application.Contracts/Skywalker/Application/Services/Contracts/IApplicationService.cs
+++ src/Ddd/src/Skywalker.Ddd.Application.Contracts/Skywalker/Application/Services/Contracts/IApplicationService.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Skywalker.Lightning.Abstractions;
 
 namespace Skywalker.Application.Services.Contracts
 {
     /// <summary>
     /// This interface must be implemented by all application services to register and identify them by convention.
     /// </summary>
-    public interface IApplicationService : ILightningService, ITransientDependency
+    public interface IApplicationService : ITransientDependency
     {
 
     }
