c303fa2240ebde754c71f6d2cfe29ffecc4a37c4
dengxuan <dengxuan@mail.com>
2021-02-01 20:11:41 +0800

    异步方法代理

--- src/Aspects/src/Skywalker.Aspects.Abstractions/Skywalker/Aspects/Interceptors/InvocationContext.cs
+++ src/Aspects/src/Skywalker.Aspects.Abstractions/Skywalker/Aspects/Interceptors/InvocationContext.cs
@@ -1,4 +1,5 @@
 ﻿using Skywalker.Aspects.DynamicProxy;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Skywalker.Aspects.Interceptors
@@ -8,13 +9,38 @@ namespace Skywalker.Aspects.Interceptors
     /// </summary>
     public class InvocationContext
     {
+
+        private MethodInfo? methodInfo = typeof(InvocationContext).GetMethod("HandleAsync", BindingFlags.Instance | BindingFlags.Public);
+
+        private void HandleAsyncWithReflection(IInvocation invocation)
+        {
+            var resultType = invocation.Method.ReturnType.GetGenericArguments()[0];
+            var mi = methodInfo?.MakeGenericMethod(resultType);
+            invocation.ReturnValue = mi.Invoke(this, new[] { invocation.ReturnValue });
+        }
+
+        //构造等待返回值的异步方法
+        public async Task<T> HandleAsync<T>(Task<T> task)
+        {
+            var t = await task;
+
+            //Do arter
+
+            return t;
+        }
+
         public IInvocation Invocation { get; }
 
         public InvocationContext(IInvocation invocation) => Invocation = invocation;
 
         public async Task ProceedAsync()
         {
-            await Task.Run(() => Invocation.Proceed());
+            Invocation.Proceed();
+            var returnType = Invocation.Method.ReflectedType;     //获取被代理方法的返回类型
+            if (returnType != null && returnType.GetGenericTypeDefinition() == typeof(Task<>))
+            {
+                HandleAsyncWithReflection(Invocation);
+            }
         }
     }
 }
