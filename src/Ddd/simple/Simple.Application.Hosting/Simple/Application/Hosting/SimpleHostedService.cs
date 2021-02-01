969f39f02b8745179777c59156c09bde694a08f5
dengxuan <dengxuan@mail.com>
2021-02-01 23:53:10 +0800

    a

---
+++ src/Ddd/simple/Simple.Application.Hosting/Simple/Application/Hosting/SimpleHostedService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Simple.Application.Abstractions;
+using Simple.Application.Users;
+using Skywalker.Ddd.Queries.Abstractions;
+using Skywalker.Ddd.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Simple.Application.Hosting
+{
+    public class SimpleHostedService : IHostedService
+    {
+        private readonly ISimpleUserApplicationService _searcher;
+
+        public SimpleHostedService(ISimpleUserApplicationService searcher)
+        {
+            _searcher = searcher;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                List<UserDto> userDtos = await _searcher.FindUsersAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            cancellationToken.WaitHandle.WaitOne();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
