a75c3bf248f7e7a3e6535013a2f8e7270a96a9e8
dengxuan <dengxuan@mail.com>
2021-02-02 00:07:45 +0800

    IServiceCollection扩展

--- src/Extensions/src/Skywalker.Extensions.Universal/Microsoft/Extensions/DependencyInjection/IServiceCollectionExtensions.cs
+++ src/Extensions/src/Skywalker.Extensions.Universal/Microsoft/Extensions/DependencyInjection/IServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using System;
