12e72ccf4744f481a93abe9cfd7fea1729e82289
dengxuan <dengxuan@mail.com>
2021-02-11 16:40:28 +0800

    修改IServiceCollection扩展函数GetSingletonInstanceOrNull 代码错误

--- src/Extensions/src/Skywalker.Extensions.Universal/Microsoft/Extensions/DependencyInjection/IServiceCollectionExtensions.cs
+++ src/Extensions/src/Skywalker.Extensions.Universal/Microsoft/Extensions/DependencyInjection/IServiceCollectionExtensions.cs
@@ -10,8 +10,6 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static T? GetSingletonInstanceOrNull<T>(this IServiceCollection services)
         {
-           var service= services.FirstOrDefault(d => d.ServiceType == typeof(T));
-            return service?.
             return (T)services
                 .FirstOrDefault(d => d.ServiceType == typeof(T))
                 ?.ImplementationInstance;
