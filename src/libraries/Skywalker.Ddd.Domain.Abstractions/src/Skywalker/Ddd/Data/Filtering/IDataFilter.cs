e355957dd0889690e0b7243ecd1825e259c8c224
dengxuan <dengxuan@mail.com>
2022-02-28 18:16:00 +0800

    去掉Domain层代码生成器

--- src/libraries/Skywalker.Ddd.Domain.Abstractions/src/Skywalker/Ddd/Data/Filtering/IDataFilter.cs
+++ src/libraries/Skywalker.Ddd.Domain.Abstractions/src/Skywalker/Ddd/Data/Filtering/IDataFilter.cs
@@ -1,26 +1,26 @@
-﻿using System;
+﻿// Licensed to the Gordon under one or more agreements.
+// Gordon licenses this file to you under the MIT license.
 
-namespace Skywalker.Ddd.Data.Filtering
+namespace Skywalker.Ddd.Data.Filtering;
+
+public interface IDataFilter<TFilter>
+    where TFilter : class
 {
-    public interface IDataFilter<TFilter>
-        where TFilter : class
-    {
-        IDisposable Enable();
+    IDisposable Enable();
 
-        IDisposable Disable();
+    IDisposable Disable();
 
-        bool IsEnabled { get; }
-    }
+    bool IsEnabled { get; }
+}
 
-    public interface IDataFilter
-    {
-        IDisposable Enable<TFilter>()
-            where TFilter : class;
+public interface IDataFilter
+{
+    IDisposable Enable<TFilter>()
+        where TFilter : class;
 
-        IDisposable Disable<TFilter>()
-            where TFilter : class;
+    IDisposable Disable<TFilter>()
+        where TFilter : class;
 
-        bool IsEnabled<TFilter>()
-            where TFilter : class;
-    }
+    bool IsEnabled<TFilter>()
+        where TFilter : class;
 }
