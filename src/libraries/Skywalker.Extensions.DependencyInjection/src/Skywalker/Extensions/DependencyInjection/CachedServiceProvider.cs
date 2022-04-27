b19c1fa15db22d12ffa57e97ca0618cbf1cec509
dengxuan <dengxuan@mail.com>
2022-04-27 15:21:24 +0800

    添加带缓存功能的serviceprovider

---
+++ src/libraries/Skywalker.Extensions.DependencyInjection/src/Skywalker/Extensions/DependencyInjection/CachedServiceProvider.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the Gordon under one or more agreements.
+// Gordon licenses this file to you under the MIT license.
+
+using Skywalker.Extensions.DependencyInjection.Abstractions;
+
+namespace Skywalker.Extensions.DependencyInjection;
+
+public class CachedServiceProvider : ICachedServiceProvider, IScopedDependency
+{
+    protected IServiceProvider ServiceProvider { get; }
+
+    protected IDictionary<Type, object> CachedServices { get; }
+
+    public CachedServiceProvider(IServiceProvider serviceProvider)
+    {
+        ServiceProvider = serviceProvider;
+
+        CachedServices = new Dictionary<Type, object>
+            {
+                {typeof(IServiceProvider), serviceProvider}
+            };
+    }
+
+    public object GetService(Type serviceType)
+    {
+        return CachedServices.GetOrAdd(serviceType, () => ServiceProvider.GetService(serviceType));
+    }
+}
