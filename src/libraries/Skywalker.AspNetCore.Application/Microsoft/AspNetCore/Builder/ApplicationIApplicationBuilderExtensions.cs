af83cb7f6236a551f1a2096644237f94073c20fb
Gordon <dengxuan@mail.com>
2022-05-05 17:58:59 +0800

    添加Application的AspNetCore IApplicationBuilder扩展

---
+++ src/libraries/Skywalker.AspNetCore.Application/Microsoft/AspNetCore/Builder/ApplicationIApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Skywalker.Ddd.Application.Pipeline.Abstractions;
+
+namespace Microsoft.AspNetCore.Builder;
+
+public static class ApplicationIApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseApplication(this IApplicationBuilder builder, Action<IPipelineChainBuilder> options)
+    {
+        var chainBuilder = builder.ApplicationServices.GetRequiredService<IPipelineChainBuilder>();
+        options?.Invoke(chainBuilder);
+        return builder;
+    }
+}
