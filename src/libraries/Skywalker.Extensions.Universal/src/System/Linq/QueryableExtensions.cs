2997f06fed9601d91c8eed89f6765d7a24d43fc4
Gordon <dengxuan@mail.com>
2022-05-16 14:55:30 +0800

    IQueryable添加WhereIf扩展函数

--- src/libraries/Skywalker.Extensions.Universal/src/System/Linq/QueryableExtensions.cs
+++ src/libraries/Skywalker.Extensions.Universal/src/System/Linq/QueryableExtensions.cs
@@ -19,5 +19,33 @@ public static class QueryableExtensions
     {
         return Task.Run(() => source.ToPagedList(skip, limit));
     }
+
+    /// <summary>
+    /// Filters a <see cref="IEnumerable{T}"/> by given predicate if given condition is true.
+    /// </summary>
+    /// <param name="source">Enumerable to apply filtering</param>
+    /// <param name="condition">A boolean value</param>
+    /// <param name="predicate">Predicate to filter the enumerable</param>
+    /// <returns>Filtered or not filtered enumerable based on <paramref name="condition"/></returns>
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, Func<T, bool> predicate)
+    {
+        return condition
+            ? source.Where(predicate).AsQueryable()
+            : source;
+    }
+
+    /// <summary>
+    /// Filters a <see cref="IEnumerable{T}"/> by given predicate if given condition is true.
+    /// </summary>
+    /// <param name="source">Enumerable to apply filtering</param>
+    /// <param name="condition">A boolean value</param>
+    /// <param name="predicate">Predicate to filter the enumerable</param>
+    /// <returns>Filtered or not filtered enumerable based on <paramref name="condition"/></returns>
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> source, bool condition, Func<T, int, bool> predicate)
+    {
+        return condition
+            ? source.Where(predicate).AsQueryable()
+            : source;
+    }
 }
 
