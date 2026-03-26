60ae540b47a53aa875a7ff9eaab9c581f1d7df5d
Calyx <dengxuan@mail.com>
2026-03-26 21:14:51 +0200

    refactor(P3): remove dead Threading code from Universal
    Delete AsyncHelper, TaskHelper, OneTimeRunner, and Locking/ from
    Extensions.Universal — all unused dead code. SkywalkerAsyncTimer in
    Extensions.Threading remains (actively used by RabbitMQ).

--- src/Skywalker.Extensions.Universal/Skywalker/Extensions/Threading/Locking/ILockHolder.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Skywalker.Extensions.Threading.Locking;
-
-public interface ILockHolder : IDisposable
-{
-    bool LockAcquired { get; }
-}
--- src/Skywalker.Extensions.Universal/Skywalker/Extensions/Threading/Locking/IUpgradeableLockHolder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Skywalker.Extensions.Threading.Locking;
-
-public interface IUpgradeableLockHolder : ILockHolder
-{
-    ILockHolder Upgrade();
-
-    ILockHolder Upgrade(bool waitForLock);
-}
